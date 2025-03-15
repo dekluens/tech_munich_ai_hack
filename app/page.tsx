80b17e863feaa06d8900dc3cdc11d7c1aa0826c6
David Klueners <davidklueners@gmail.com>
2025-03-15 18:44:34 +0100

    small visual improvement

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import ResourcesContainer from "@/components/resources-container";
 export default function Page() {
   return (
     <main className="flex h-screen w-full">
-      <div className="w-1/2 h-full">
+      <div className="w-1/2 h-full p-4">
         <ChatContainer />
       </div>
-      <div className="w-1/2 h-full">
+      <div className="w-1/2 h-full p-4">
         <ResourcesContainer />
       </div>
     </main>
